15c114688c06798c44167af5004cdbb63c02478b
Srikanth <noonesrikath@gmail.com>
2024-09-27 18:11:27 +0530

    vpc added

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,10 @@ resource "aws_vpc" "new-vpc" {
         Name = "new-vpc"
     }
 }
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.new-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "public-subnet"
+  }
+}
